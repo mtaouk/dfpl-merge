4a283e1d2cb0e3b036446d55013b5ad899db9f99
mtaouk <90819350+mtaouk@users.noreply.github.com>
2025-09-17 12:24:50 +1000

    Add license

--- dfpl-merge.py
+++ dfpl-merge.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python3
 
+"""
+Copyright 2025 Mona Taouk
+https://github.com/mtaouk/dfpl-merge.py
+
+This is the main script for dfpl-merge. 
+
+dfpl-merge is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by the Free Software Foundation,
+either version 3 of the License, or (at your option) any later version. dfpl-merge is distributed in
+the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+details. You should have received a copy of the GNU General Public License along with dfpl-merge. If
+not, see <http://www.gnu.org/licenses/>.
+"""
 
 # ---- imports ---- 
 import argparse # parse command-line flags
